43d202f88e02d4d76cec1157fa6f882d0b82b56b
Ida Midžić <idmidzic@vsite.hr>
2021-11-30 14:30:16 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace WindowsFormsAppOsvojiLoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.brojevi = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(29, 37);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(126, 23);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "Izaberi brojeve";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // txt_1
+            // 
+            this.txt_1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_1.Location = new System.Drawing.Point(180, 27);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.ReadOnly = true;
+            this.txt_1.Size = new System.Drawing.Size(470, 31);
+            this.txt_1.TabIndex = 1;
+            this.txt_1.TextChanged += new System.EventHandler(this.txt_1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(619, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 217);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(479, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Poništi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // brojevi
+            // 
+            this.brojevi.Location = new System.Drawing.Point(121, 247);
+            this.brojevi.Name = "brojevi";
+            this.brojevi.Size = new System.Drawing.Size(196, 20);
+            this.brojevi.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(216, 250);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.brojevi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txt_1);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
+            this.Text = "Osvoji Loto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox brojevi;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppOsvojiLoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            // 6 kuglica, 1-36
+            int donji = 1;
+            int gornji = 36;
+            int[] slucajniBrojevi = new int[6];
+            bool zastavica = false;
+
+
+            // Promijeniti tekst na Buttonu klikni: Klikni za novo izvačenje
+            // idx globalna varijabla
+            // automatsko izvlačenje sve odjednom u 1 buttonu
+            // zvukovi ting ting kad se izvuče broj, pauza između izvlačenja
+            // sporo izvlačenje
+            // controls of array
+
+
+            TextBox[] brojevi = new TextBox[6];
+
+
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+            for (int idx = 0; idx < 6; idx++)
+            {
+                zastavica = false;
+
+                do
+                {
+
+                    slucajniBrojevi[idx] = rnd.Next(donji, gornji);
+               
+
+                    if (idx > 0)
+                    {
+
+                        for (int jdx = 0; jdx == idx; jdx++)
+                        {
+
+                            if (slucajniBrojevi[jdx] == slucajniBrojevi[idx])
+                            {
+                                zastavica = true;
+                                // break;
+                            }
+
+                        }
+                    }
+                } while (zastavica == true);
+
+                listBox1.Items.Add(slucajniBrojevi[idx]);
+                int broj = slucajniBrojevi[idx];
+                
+                txt_1.Text = txt_1.Text + broj.ToString() +"     "; 
+            }
+                        
+           //int len = slucajniBrojevi.Length; 
+            
+            //listBox1.Items.Add(len);
+            //string str = broj.ToString();
+
+            //foreach (string i in myArray2)
+            //{
+            //    myArray2[0] = str;
+            //    listBox1.Items.Add(myArray2);
+            //}
+            //txt_1.Text = broj.ToString();
+
+            //for (int idx = 1; idx < 5; idx++)
+            //{
+            //    Random rnd = new Random();
+
+            //    int broj = rnd.Next(lower, upper);
+            //    myArray.Append(broj);
+            //    listBox1.Items.Add(txt_1.Text);
+               //txt_1.Text = broj.ToString();
+            //}
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+    }
+
+        private void txt_1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
